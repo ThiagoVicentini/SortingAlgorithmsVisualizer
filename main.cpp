e05df6b1795c6ccbc49c32979f8a01f16fb5b14f
Anakin <thiago.h.vicentini@gmail.com>
2023-12-16 00:00:22 -0300

    Create a function showArray to display the lines in the vector<Line> _array, it uses a offset to shift the draw to the center of the screen and normalize the points in the coordinate systems

--- main.cpp
+++ main.cpp
@@ -23,6 +23,21 @@ vector<Line> _array(480, {0, {1, 0, 0}});
 
 float w = 500, h = 500;
 float LO = 0.0, HI = 480.0;
+float offset = 10;
+
+void normalize(float &x1, float &y1, float &x2, float &y2){
+    x1 = 2*x1 / w - 1;
+    y1 = 2*y1 / h - 1;
+
+    x2 = 2*x2 / w - 1;
+    y2 = 2*y2 / h - 1;
+
+    float scale = 9.9;
+    x1 *= scale;
+    y1 *= scale;
+    x2 *= scale;
+    y2 *= scale;
+}
 
 void init(){
     glClearColor(0, 0, 0, 1);
@@ -36,11 +51,31 @@ void reshape(GLsizei w, GLsizei h){
     glMatrixMode(GL_MODELVIEW);
 }
 
+void showArray(){
+    float x1, y1, x2, y2;
+    for(int i=0; i<(int)_array.size(); i++){
+        x1 = i + offset;
+        y1 = 0;
+        x2 = i + offset;
+        y2 = _array[i].value;
+        normalize(x1, y1, x2, y2);
+
+        float r = _array[i].color[0];
+        float g = _array[i].color[1];
+        float b = _array[i].color[2];
+        glColor3f(r, g, b);
+        glBegin(GL_LINES);
+            glVertex2f(x1, y1);
+            glVertex2f(x2, y2);
+        glEnd();
+    }
+}
+
 void display(){ 
     glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
 
-    // Draw
+    showArray();
 
     glutSwapBuffers();
 };
